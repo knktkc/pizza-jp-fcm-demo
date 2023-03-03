18893922d557cc34be609d7037608e95de48fb06
knktkc <t.kaneko@xtone.co.jp>
2023-03-03 19:17:33 +0900

    バージョンの修正

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,9 @@
-importScripts("https://www.gstatic.com/firebasejs/7.9.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/7.9.1/firebase-messaging.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.17.1/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.17.1/firebase-messaging-compat.js"
+);
 
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
